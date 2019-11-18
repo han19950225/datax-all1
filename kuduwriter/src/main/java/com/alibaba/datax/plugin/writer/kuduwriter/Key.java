4a7ba29042ff10154ea51e6e46bb2bba519c84f1
hanbing <hanbing2018@126.com>
2019-11-18 21:49:01 +0800

    datax

---
+++ kuduwriter/src/main/java/com/alibaba/datax/plugin/writer/kuduwriter/Key.java
@@ -0,0 +1,11 @@
+package com.alibaba.datax.plugin.writer.kuduwriter;
+
+public class Key {
+
+    public static final String KEY_KUDU_TABLE = "table";  //配置中的kudu表名字
+    public static final String KEY_KUDU_COLUMN = "column";  //kudu表名字对应的读取列
+    public static final String KEY_KUDU_ENCODE = "encoding";
+    public static final  String KEY_KUDU_MASTER="master";  //配置中的kudu-master
+    public static final  String KET_KUDU_PRIMARY="primaryKey";  //kudu表的主键
+
+}
